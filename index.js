9af68f4aadf103f54a459b09e82a5ff33c0640ff
James Farris <jafarris.exe@gmail.com>
2023-05-29 00:25:46 -0400

    Added file structure and psuedo-code

---
+++ index.js
@@ -0,0 +1,19 @@
+// TODO: Add necessary packages that I need to generate the SVG
+
+// Create the following questions for the CLI
+// Three characters
+// Text color OR hexadecimal
+// Shape (this should be a list)
+// Shape color
+
+// Switch case for shape
+
+// Apply the shapes color
+
+// set text and shape of SVG
+
+// Writefile to logo.svg
+
+// Log
+
+// Error
